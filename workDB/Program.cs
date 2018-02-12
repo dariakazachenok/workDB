8725414602c0df066cc68d00f395846baabcd3d5
MashaKazachenok <kazachenokm@mail.ru>
2018-02-12 17:31:27 +0300

    second task

--- workDB/Program.cs
+++ workDB/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using workDB;
 
 namespace EFGetStarted.ConsoleApp.ExistingDb
@@ -9,9 +10,9 @@ namespace EFGetStarted.ConsoleApp.ExistingDb
         {
             using (var db = new BloggingContext())
             {
-                db.Blog.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+                /* db.Blog.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
                 var count = db.SaveChanges();
-                Console.WriteLine("{0} records saved to database", count);
+                Console.WriteLine("{0} records saved to database", count); */
 
                 Console.WriteLine();
                 Console.WriteLine("All blogs in database:");
@@ -19,7 +20,24 @@ namespace EFGetStarted.ConsoleApp.ExistingDb
                 {
                     Console.WriteLine(" - {0}", blog.Url);
                 }
+
+                var count = db.Blog.Count();
+                Console.WriteLine(" quality blogs in database: {0}", count);
+
+                var blogesCount = db.Blog.Where(b => b.Post.Count() < 1).ToList().Count();
+                Console.WriteLine(" Quality blogs in database where quality post < 1 : {0}", blogesCount);
+
+                var blogesNames = db.Blog.Where(b => b.Post.Count() < 1).Select(u => u.Url).ToList<string>();
+                Console.WriteLine("Blogs in database where quality post < 1 : {0}", String.Join(",",blogesNames));
+
             }
         }
     }
 }
+
+
+
+
+
+
+
